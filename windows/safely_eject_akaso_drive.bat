1a263480ad52d61cd5bebae3c07bac7b28f3ea31
Tudor3510 <tudorcioc@gmail.com>
2026-07-12 10:56:16 +0300

    Fix script to safely eject "akaso" labeled drive on Windows

--- windows/safely_eject_akaso_drive.bat
+++ windows/safely_eject_akaso_drive.bat
@@ -3,7 +3,7 @@ setlocal EnableExtensions EnableDelayedExpansion
 
 set "SOURCE_DRIVE="
 
-for /f "usebackq" %%D in (`powershell -NoProfile -Command "(Get-Volume ^| Where-Object { $_.FileSystemLabel -ieq 'akaso' }).DriveLetter"`) do set "SOURCE_DRIVE=%%D"
+for /f "usebackq" %%D in (`powershell -NoProfile -Command "(Get-Volume | Where-Object { $_.FileSystemLabel -ieq 'akaso' }).DriveLetter"`) do set "SOURCE_DRIVE=%%D"
 
 if not defined SOURCE_DRIVE (
 	echo Drive labeled "akaso" was not found.
@@ -11,6 +11,6 @@ if not defined SOURCE_DRIVE (
 )
 
 echo Ejecting drive !SOURCE_DRIVE!:
-powershell -NoProfile -Command "(New-Object -ComObject Shell.Application).Namespace(17).ParseName('!SOURCE_DRIVE!:').InvokeVerb('Eject')"
+powershell -NoProfile -Command "(New-Object -ComObject Shell.Application).Namespace(17).ParseName('!SOURCE_DRIVE!:').InvokeVerb('Eject'); Start-Sleep -Seconds 2"
 
 endlocal
